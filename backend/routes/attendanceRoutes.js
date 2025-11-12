6caca8b44828d3ae00ff112b32fe73a2526018c8
shohith-git <shohithkumar.cs.nc@cambridge.edu.in>
2025-11-12 23:41:05 +0530

    modification of the attendance

--- backend/routes/attendanceRoutes.js
+++ backend/routes/attendanceRoutes.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const attendanceController = require("../controllers/attendanceController");
+const { authMiddleware, isWarden } = require("../middleware/authMiddleware");
+
+router.get(
+  "/summary",
+  authMiddleware,
+  isWarden,
+  attendanceController.getDailySummary
+);
+router.get(
+  "/date/:date",
+  authMiddleware,
+  isWarden,
+  attendanceController.getAttendanceByDateFull
+);
 
 // Mark attendance
 router.post("/", attendanceController.markAttendance);
